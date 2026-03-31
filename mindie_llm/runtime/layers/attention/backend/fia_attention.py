Fix block calculation
Co-authored-by: gengli8<gengli8@huawei.com>



# message auto-generated for no-merge-commit merge:
!446 merge devfix into dev

[aclgraph] Fix block calculation

Created-by: forcekeng
Commit-by: gengli8
Merged-by: ascend-robot
Description: <!--
PR描述模板更新日期：20251225
-->

# 合入背景
> 请描述为什么要做这个PR内的改动。\
> 如涉及，请关联前序PR或同特性/需求下的其他PR。\
> 如果是修复之前PR引入的问题，请关联引入问题的PR。\
> 注意：`Fixes #ISSUE ID`会自动关闭issue，如问题部分解决请不要使用`Fixes`，可以用`Fix part of #ISSUE ID`替代.

# 修改内容
max_block_per_seq 传递流程：
> - `class AclGraphModelWrapperExp  __init__` 获取generator传递的 `_max_seq_len` 和 `_block_size `，并计算出 `max_block_per_seq`。
> - `class ModelRunnerExp _init_buffer` 将 `max_block_per_seq` 传递给 `class ForwardContext register`。
> -  `class ForwardContext register`将`max_block_per_seq`传递给 `class SfaMetadata register_buffer`和 `class FiaAttentionMetadata register_buffer`，用于注册 `block_tables`。

# 资料变更
> 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”。

# 接口变更
> 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”。

# 测试结果
> 请说明测试场景，测试方法以及测试结果。\
> 测试用例设计时需考虑硬件、部署方式、功能、性能、精度、显存等维度。

# CheckList
> PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]。

- [ ] 代码注释完备
- [ ] 正确记录错误日志
- [ ] 进行了返回值校验 (禁止使用void屏蔽安全函数、自研函数返回值；考虑接口的异常场景；调用底层组件接口时，需要进行返回值校验)
- [ ] 进行了空指针校验
- [ ] 若存在资源申请，使用后资源被正确的释放了
- [ ] 若涉及多线程场景，考虑了并发场景，不存在死锁问题
- [ ] 按照[代码仓中提供的格式模板](https://gitcode.com/Ascend/MindIE-LLM/blob/master/.clang-format)，使用clang-format工具格式化代码
- [ ] 符合Ascend社区的编码规范。[C++ 语言编程指导](https://gitcode.com/Ascend/community/blob/master/docs/contributor/Ascend-cpp-coding-style-guide.md) | [C++ 语言安全编程指导](https://gitcode.com/Ascend/community/blob/master/docs/contributor/Ascend-cpp-secure-coding-guide.md)


See merge request: Ascend/MindIE-LLM!446

mindie_llm/runtime/layers/attention/backend/fia_attention.py
@@ -70,9 +70,10 @@ class FiaAttentionMetadata(AttentionMetadata):
         )
 
     @staticmethod
-    def register_buffer(max_num_token, device):
+    def register_buffer(max_num_token, device, max_block_per_seq):
         input_buffer.register("seq_lens", torch.zeros(max_num_token, dtype=torch.int32, device=device))
-        input_buffer.register("block_tables", torch.zeros((max_num_token, 64), dtype=torch.int32, device=device))
+        input_buffer.register("block_tables", 
+                              torch.zeros((max_num_token, max_block_per_seq), dtype=torch.int32, device=device))
         input_buffer.register("slot_mapping", -torch.ones(max_num_token, dtype=torch.int32, device=device))
 
     def to_device(self, device):

mindie_llm/runtime/model_runner/forward_context_exp.py
@@ -82,19 +82,23 @@ class ForwardContext:
     sub_forward_context = None
 
     @staticmethod
-    def register(max_num_token: int, device: torch.device, hf_config: HuggingFaceConfig) -> None:
+    def register(max_num_token: int,
+                 device: torch.device,
+                 hf_config: HuggingFaceConfig,
+                 max_block_per_seq: int) -> None:
         """Register buffer for attention metadata.
         
         Args:
             max_num_token: Maximum number of tokens.
             device: Target device.
             hf_config: HuggingFaceConfig of model.
+            max_block_per_seq: Maximum number of blocks per sequence.
         """
         attns = get_global_attn_dict()
         attn_layer = attns[next(iter(attns))]
         attn_backend = attn_layer.get_attn_backend()
         metadata_cls = attn_backend.get_builder_cls().get_metadata_cls()
-        metadata_cls.register_buffer(max_num_token, device)
+        metadata_cls.register_buffer(max_num_token, device, max_block_per_seq)
         MtpMetadata.register_buffer(max_num_token, device, hf_config)
 
     def to_device(self, device: torch.device) -> None:

mindie_llm/runtime/model_runner/model_runner_exp.py
@@ -124,6 +124,12 @@ class ModelRunnerExp:
         local_rank = local_rank if local_rank is not None else rank
         self.device = set_device(rank, npu_id if npu_id is not None else local_rank)
         self._max_seq_len = kwargs.get("max_seq_len", -1)
+        self._block_size = kwargs.get("block_size", 128)
+        if self._max_seq_len <= 0:
+            raise ValueError(f"max_seq_len must be specified and greater than 0, but got {self._max_seq_len}")
+        if self._block_size <= 0:
+            raise ValueError(f"block_size must be greater than 0, but got {self._block_size}")
+        self._max_block_per_seq = (self._max_seq_len + self._block_size - 1) // self._block_size
         self.is_draft_model = kwargs.get("is_draft_model", False)
 
         # bin cpus to the NUMA
@@ -412,7 +418,8 @@ class ModelRunnerExp:
 
     def _init_buffer(self) -> None:
         """Initialize input buffers for graph mode."""
-        ForwardContext.register(self._max_num_token, self.device, self._mindie_llm_config.hf_config)
+        ForwardContext.register(self._max_num_token, self.device, self._mindie_llm_config.hf_config,
+            self._max_block_per_seq)
         input_buffer.register("input_ids", torch.zeros(self._max_num_token, dtype=torch.int32, device=self.device))
         input_buffer.register("position_ids", torch.zeros(self._max_num_token, dtype=torch.int64, device=self.device))
 